ba2a48a07bb93a88222797d9584fc74627f43eac
Eli Selkin <ejselkin@cpp.edu>
2018-04-03 23:56:05 -0700

    update useradd... well maybe it was the createdb

--- setup_postgres.sh
+++ setup_postgres.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #start
 createuser -p 9020 -d -l -E testing
-psql -p 9020 -c "alter ROLE testing WITH password 'testing'"
 createdb -p 9020 -O testing CurbMapUsers
+psql -p 9020 -c "alter ROLE testing WITH password 'testing'"
 psql -p 9020 -f /builddb.sql CurbMapUsers
